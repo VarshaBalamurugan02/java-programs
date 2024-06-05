7a745af0f7bd1bb24ed5c783b325f35142aced89
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:20:19 +0530

    Add files via upload

---
+++ forloop.java
@@ -0,0 +1,21 @@
+package javaprogram;
+
+public class forloop {
+	int i,j;
+	void sample()
+	{
+		for (i=0;i<=5;i++)
+		{
+			System.out.println("I value is::"+i);
+		}
+	}
+	
+
+	public static void main(String[] args) {
+		forloop f1=new forloop();
+		f1.sample();
+		// TODO Auto-generated method stub
+
+	}
+
+}
