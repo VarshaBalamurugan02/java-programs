6e9e7858fb47a7c6d1ebd564e062bff18de8da54
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:41:39 +0530

    Add files via upload

---
+++ sample20.java
@@ -0,0 +1,14 @@
+package javaprogram;
+import java.util.Scanner;
+
+
+
+public class sample20 {
+	Scanner sc=new Scanner
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
