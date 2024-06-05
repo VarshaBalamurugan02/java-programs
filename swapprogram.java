6ebbbcb5edff498314bc613a2a6a1a162f50bb60
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:47:17 +0530

    Add files via upload

---
+++ swapprogram.java
@@ -0,0 +1,21 @@
+package javaprogram;
+
+public class swapprogram {
+	void sample()
+	{
+		int a=10,b=20,t;
+		t=a;
+		a=b;
+		b=t;
+		System.out.println(" the value is::"+a+""+b);
+	}
+	
+	public static void main(String []args) {
+		swapprogram f1=new swapprogram();
+		f1.sample();
+		
+		
+	}
+	
+
+}
