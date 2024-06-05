9c1754c393947baa064de3d5c06ed7440ba9bea6
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:45:19 +0530

    Add files via upload

---
+++ string.java
@@ -0,0 +1,14 @@
+package javaprogram;
+
+public class string {
+	
+
+	public static void main(String[] args) {
+		String s1="welcome";
+		String s2=new String("welcome");
+		System.out.println("s1 value is::"+s1);
+		System.out.println("s2 value is::"+s2);
+
+	}
+
+}
