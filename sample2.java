501fa4a41f87d7e3d9a927638c1b5525a9d643dd
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:30:19 +0530

    Add files via upload

---
+++ sample2.java
@@ -0,0 +1,21 @@
+package javaprogram;
+
+public class sample2 {
+	void get1()
+	{
+		System.out.println("method-1");
+	}
+    void get2()
+    {
+    	System.out.println("method-2");
+    }
+	public static void main(String[] args) {
+		sample2 d1=new sample2();
+		sample2 d2=new sample2();
+		d1.get1();
+		d2.get2();
+		
+
+	}
+
+}
