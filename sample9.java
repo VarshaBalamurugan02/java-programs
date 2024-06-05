c17756fbbb0662aaf1f43542e463402d117d8633
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:34:47 +0530

    Add files via upload

---
+++ sample9.java
@@ -0,0 +1,19 @@
+package javaprogram;
+
+public class sample9 {
+	void bitwiseoperator()
+	{
+		int a=2,b=3;
+		System.out.println("BitwiseAND::"+(a&b));
+		System.out.println("BitwiseOR::"+(a|b));
+		
+	}
+
+	public static void main(String[] args) {
+		sample9 f1=new sample9();
+		f1.bitwiseoperator();
+		// TODO Auto-generated method stub
+
+	}
+
+}
