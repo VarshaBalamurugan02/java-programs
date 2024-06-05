c07fb45bb2ee2ba2d7a91dbf4d8b5dc53f268c3f
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:45:44 +0530

    Add files via upload

---
+++ string2.java
@@ -0,0 +1,14 @@
+package javaprogram;
+
+public class string2 {
+	
+
+	public static void main(String[] args) {
+		String s1="welcome";
+		
+		System.out.println("s1 length is::"+s1.length());
+		System.out.println("s1 value CharAt is::"+ s1.charAt(4));
+
+	}
+
+}
