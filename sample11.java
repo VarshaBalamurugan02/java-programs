cefadd5ab5e086b027ddf54d994a251b62434013
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:35:34 +0530

    Add files via upload

---
+++ sample11.java
@@ -0,0 +1,16 @@
+package javaprogram;
+
+public class sample11 {
+	void bitwiseoperator()
+	{
+		int a=2,b=3;
+		System.out.println("XOR::"+(a^b));
+		}
+
+	public static void main(String[] args) {
+		sample11 f1=new sample11();
+		f1.bitwiseoperator();
+	
+	}
+
+}
