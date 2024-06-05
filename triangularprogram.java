d83f448f115a34f209879232c6edb01235c57481
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:49:05 +0530

    Add files via upload

---
+++ triangularprogram.java
@@ -0,0 +1,23 @@
+package javaprogram;
+import java.util.Scanner;
+
+public class triangularprogram {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Enter the number of rows: ");
+        int rows = scanner.nextInt();
+
+        for (int i = 1; i <= rows; i++) {
+            
+            for (int j = 1; j <= rows - i; j++) {
+                System.out.print("  ");
+            }
+            for (int k = 1; k <= i; k++) {
+                System.out.print(k + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
+
