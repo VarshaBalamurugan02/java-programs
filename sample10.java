d2d9e09254cdc3c346b6a8b436c1196ede20a8bc
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:35:10 +0530

    Add files via upload

---
+++ sample10.java
@@ -0,0 +1,19 @@
+package javaprogram;
+
+public class sample10 {
+		void bitwiseoperator()
+		{
+			int a=2,b=3;
+			System.out.println("LeftShift::"+(a<<2));
+			System.out.println("RightShift::"+(a>>1));
+			
+		}
+
+	public static void main(String[] args) {
+		sample10 f1=new sample10();
+		f1.bitwiseoperator();
+		// TODO Auto-generated method stub
+
+	}
+
+}
