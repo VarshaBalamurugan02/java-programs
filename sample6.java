60da88475c710164b48a8f83fb0d988fc1f57f42
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:33:37 +0530

    Add files via upload

---
+++ sample6.java
@@ -0,0 +1,20 @@
+package javaprogram;
+
+public class sample6 {
+	void comparisonoperator()
+	{
+		int a2=200,b2=100;
+		System.out.println("Lessthan::"+(a2<b2));
+		System.out.println("Greaterthan::"+(a2>b2));
+		System.out.println("Equalto::"+(a2==b2));
+		System.out.println("NotEqualto::"+(a2!=b2));
+	}
+
+	public static void main(String[] args) {
+		sample6 f1=new sample6();
+		f1.comparisonoperator();
+		// TODO Auto-generated method stub
+
+	}
+
+}
