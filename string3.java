bc84f56949a3a9a0ffd091baa61ad61b93da4dfc
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:46:04 +0530

    Add files via upload

---
+++ string3.java
@@ -0,0 +1,20 @@
+package javaprogram;
+
+public class string3 {
+	
+
+	public static void main(String[] args) {
+		String s1="welcome";
+		String s2=new String("welcome");
+		if(s1.equals("welcome")&&s2.equals("welcome")){
+			System.out.println("LOGIN SUCEESS!!!");
+			
+		}
+		else
+		{
+			System.out.println("LOGIN FAIL!!!");
+		}
+
+	}
+
+}
