4b489123ee70c5deee3f829bcadc65b57f895b6e
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-05 13:31:36 +0530

    Add files via upload

---
+++ sample5.java
@@ -0,0 +1,20 @@
+package javaprogram;
+
+public class sample5 {
+        void arithmeticoperators()
+        {
+        	int a1=100,b1=200;
+        	System.out.println("Add::"+(a1+b1));
+        	System.out.println("mul::"+(a1*b1));
+        	System.out.println("div::"+(a1/b1));
+        	System.out.println("module::"+(a1%b1));
+        }
+	public static void main(String[] args) {
+		sample5 f1=new sample5();
+		f1.arithmeticoperators();
+	
+		// TODO Auto-generated method stub
+
+	}
+
+}
