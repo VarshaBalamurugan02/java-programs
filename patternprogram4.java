1b66b5a2d914f279279d9fc98aac22204a3522a5
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-19 11:21:34 +0530

    Add files via upload

---
+++ patternprogram4.java
@@ -0,0 +1,20 @@
+package javaprogram;
+
+public class patternprogram4 {
+
+	public static void main(String[] args) {
+		int n=5, i,j;
+            for(i=0;i<n;i++) {
+            	for(j=0;j<n;j++) {
+            		if(i==0||j==0||i==n/2||j==n-1) {
+            		System.out.print("*");
+            	}
+            		else {
+            			System.out.print(" ");
+            		}
+            }
+            	System.out.println("");
+	}
+
+}
+}
