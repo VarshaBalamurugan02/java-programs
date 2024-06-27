9cf06900553ee465bdf7679900eb1eb2da715ecc
VarshaBalamurugan02 <161809819+VarshaBalamurugan02@users.noreply.github.com>
2024-06-27 10:42:15 +0530

    Add files via upload

---
+++ defaultconstructor.java
@@ -0,0 +1,14 @@
+package javaprogram;
+class bike{
+	bike(){
+		System.out.println("bike is created");
+	}
+}
+public class defaultconstructor {
+
+	public static void main(String[] args) {
+		bike b=new bike();
+
+	}
+
+}
